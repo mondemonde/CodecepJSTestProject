45acf7470a11bd0f7c92a3b256b5e857206d1727
jelmarose <jelmarosedevera@gmail.com>
2019-07-19 15:54:33 +0800

    Updated tests

---
+++ TestConsoleApp/TestCases/About.md
@@ -0,0 +1,3 @@
+The tests here are run using CodeceptJS + Puppeteer.
+
+For a codeceptjs only repo, clone: https://github.com/jelmarose/CodeceptJSTests
--- TestConsoleApp/TestCases/NavigateHome_test.js
+++ TestConsoleApp/TestCases/NavigateHome_test.js
@@ -6,6 +6,7 @@ Scenario('Go to Leaderboard', (I) => {
     I.see('Leaderboard');
     I.clickLink('//html/body/section[1]/div/div/div/ul/li[2]/a');
     I.waitForText('This Weeks Stars');
+    I.wait(3);
 });
 
 Scenario('Go to Activity', (I) => {
@@ -14,4 +15,31 @@ Scenario('Go to Activity', (I) => {
     I.see('Activity');
     I.clickLink('//html/body/section[1]/div/div/div/ul/li[3]/a');
     I.waitForText('is a new member in the forum')
+    I.wait(3);
+})
+
+Scenario('Go to Badges', (I) => {
+    I.amOnPage('http://devforum.azurewebsites.net/');
+    I.waitForText('Badges');
+    I.see('Badges');
+    I.clickLink('//html/body/section[1]/div/div/div/ul/li[4]/a');
+    I.waitForText('Photogenic')
+    I.wait(3);
+})
+
+Scenario('View posts tagged with QUICKREACH', (I) => {
+    I.amOnPage('http://devforum.azurewebsites.net/');
+    I.waitForText('QUICKREACH');
+    I.see('QUICKREACH');
+    I.clickLink('//*[@id="main"]/div/div[2]/div/div[3]/ul/li[1]/span/a');
+    I.waitForText('Discussions Tagged With:quickreach')
+    I.wait(3);
+})
+
+Scenario('Search posts with query "test"', (I) => {
+    I.amOnPage('http://devforum.azurewebsites.net/');
+    I.waitForElement('//*[@id="term"]');
+    I.fillField('//*[@id="term"]', 'test');
+    I.pressKey('Enter');
+    I.wait(3);
 })
